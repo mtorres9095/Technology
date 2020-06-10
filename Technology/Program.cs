3043c9c1edf836f7f843971082494b85ea78c454
mtorres9095 <mtorres9095@gmail.com>
2020-06-10 11:39:17 -0400

    Established the Solution and 3 classes

--- Technology/Program.cs
+++ Technology/Program.cs
@@ -6,7 +6,20 @@ namespace Technology
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        public string ItemLocation { get; set; }
+        public int RamMemory { get; set; }
+        public int StorageCapacity { get; set; }
+
+        public Program(string itemLocation, int ramMemory, int storageCapacity)
+        {
+            ItemLocation = itemLocation;
+            RamMemory = ramMemory;
+            StorageCapacity = storageCapacity;
         }
+        }
+        public readonly PrintItemLocation()
+    {
+        Console.WriteLine($"The item you're looking for is located at:"+ itemLocation");
+    }
     }
 }
